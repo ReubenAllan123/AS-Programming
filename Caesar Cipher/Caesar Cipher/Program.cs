c658dc0ee332833fc06cc8b8868ee159cd3d7294
07ReubenAllan <07ReubenAllan@IOMG21680W>
2019-11-12 15:42:42 +0000

    need to get encrypt/decrypt to work alternating chars

--- Caesar Cipher/Caesar Cipher/Program.cs
+++ Caesar Cipher/Caesar Cipher/Program.cs
@@ -10,7 +10,88 @@ namespace Caesar_Cipher
     {
         static void Main(string[] args)
         {
-            Console.ReadLine();
+            string Message = Encrypt("Cheese Please!", 15, 20);
+            Console.WriteLine(Message);
+            Console.WriteLine(Decrypt(Message, 15, 20));
+            Console.ReadKey();
+        }
+        public static string Encrypt(string plaintext, int key1, int key2)
+        {
+            string tempChars = "";
+            for (int i = 0; i < plaintext.Length; i++)
+            {
+                int asciiCode = (int)plaintext[i];
+                if (char.IsUpper(plaintext[i]))
+                {
+                    asciiCode = asciiCode - 65;
+                    asciiCode = asciiCode + ShiftIt(key1, key2, i, asciiCode);                    
+                    asciiCode = asciiCode % 26;
+                    asciiCode = asciiCode + 65;
+                }
+                else if (char.IsLower(plaintext[i]))
+                {
+                    asciiCode = asciiCode - 97;
+                    asciiCode = asciiCode + ShiftIt(key1, key2, i, asciiCode);
+                    asciiCode = asciiCode + 26;
+                    asciiCode = asciiCode % 26;
+                    asciiCode = asciiCode + 97;
+                }
+                tempChars = tempChars + (char)asciiCode;
+            }
+            return tempChars;
+        }
+        public static string Decrypt(string cipherText, int key1, int key2)
+        {
+            string tempChars = "";
+            for (int i = 0; i < cipherText.Length; i++)
+            {
+                int asciiCode = (int)cipherText[i];
+                if (char.IsUpper(cipherText[i]))
+                {
+                    asciiCode = asciiCode - 65;
+                    if (i % 2 == 0)
+                    {
+                        asciiCode = asciiCode - key1;
+                    }
+                    else
+                    {
+                        asciiCode = asciiCode - key2;
+                    }
+                    asciiCode = asciiCode + 26;
+                    asciiCode = asciiCode % 26;
+                    asciiCode = asciiCode + 65;
+                    
+                }
+                else if (char.IsLower(cipherText[i]))
+                {
+                    asciiCode = asciiCode - 97;
+                    if (i % 2 == 0)
+                    {
+                        asciiCode = asciiCode - key1;
+                    }
+                    else
+                    {
+                        asciiCode = asciiCode - key2;
+                    }
+                    asciiCode = asciiCode + 26;
+                    asciiCode = asciiCode % 26;
+                    asciiCode = asciiCode + 97;
+                }
+                tempChars = tempChars + (char)asciiCode;
+            }
+            return tempChars;
+        }
+        public static int ShiftIt(int key1, int key2, int i, int asciiCode)
+        {
+            if (i % 2 == 0)
+            {
+                asciiCode = asciiCode + key1;
+            }
+            else
+            {
+                asciiCode = asciiCode + key2;
+            }
+            return asciiCode;
         }
     }
 }
