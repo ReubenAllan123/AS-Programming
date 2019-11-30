8831fe4613cbde2435dbbcf7a4477f3520a72a76
Reuben Allan <reubenallan@online.sch.im>
2019-11-30 17:35:59 +0000

    complete circular queue

--- circularQueue/circularQueue/Program.cs
+++ circularQueue/circularQueue/Program.cs
@@ -13,19 +13,16 @@ namespace Queue
             string[] theQueue = new string[10];
             int NFS = 0;
             int FOQ = 0;
-            bool queueEmpty = true;
-            bool queueFull = false;
             bool running = true;
-
             while (running == true)
             {
                 switch (choice())
                 {
                     case (1):
-                        add(ref theQueue, ref NFS, ref queueEmpty, ref queueFull, FOQ);
+                        add(ref theQueue, ref NFS, FOQ);
                         break;
                     case (2):
-                        remove(ref theQueue, ref NFS, ref FOQ, ref queueEmpty, ref queueFull);
+                        remove(ref theQueue, ref NFS, ref FOQ);
                         break;
                     case (3):
                         output(theQueue);
@@ -51,39 +48,34 @@ namespace Queue
             int.TryParse(Console.ReadLine(), out int input);
             return input;
         }
-        public static void add(ref string[] theQueue, ref int NFS, ref bool queueEmpty, ref bool queueFull, int FOQ)
-        {
-            Console.Write("Input data to add to the queue: ");
-            theQueue[NFS] = Console.ReadLine();
-            queueEmpty = false;
-            NFS++;
-            if (NFS == FOQ)
-            {
-                queueFull = true;
-                Console.Write("ERROR: Queue is full, remove from Queue.");
-            }
+        public static void add(ref string[] theQueue, ref int NFS,int FOQ)
+        {        
             if (NFS > 9)
             {
                 NFS = 0;
-            }           
+            }
+            Console.Write("Input data to add to the queue: ");
+            theQueue[NFS] = Console.ReadLine();  
+            NFS++;        
             Console.ReadKey();
             Console.Clear();
         }
-        public static void remove(ref string[] theQueue, ref int NFS, ref int FOQ, ref bool queueEmpty, ref bool queueFull)
-        {
-            theQueue[FOQ] = "";               
-            Console.WriteLine("Element removed.");
-            queueFull = false;
-            FOQ++;
-            if (FOQ == NFS)
-            {
-                queueEmpty = true;
-                Console.WriteLine("ERROR: the Queue is empty, nothing to remove.");
-            }
+        public static void remove(ref string[] theQueue, ref int NFS, ref int FOQ)
+        {        
             if (FOQ > 9)
             {
                 FOQ = 0;
-            }      
+            }     
+            if (theQueue[FOQ] == "")
+            {
+                Console.WriteLine("There is no data to remove from this location.");
+            }
+            else
+            {
+            theQueue[FOQ] = "";
+            }
+            FOQ++;
+            Console.WriteLine("Element removed.");
             Console.ReadKey();
             Console.Clear();
         }
