a2ad8b45a127c22aca17fb89ca4734ebea1e5f96
07ReubenAllan <07ReubenAllan@IOMG21680W>
2019-12-06 10:25:05 +0000

    v1

---
+++ Advent1/Advent1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Advent1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal mass = 0;
+            using (StreamReader CurrentFile = new StreamReader("masses"))
+            {
+                for (int i = 0; i < 2; i++)
+                {
+                    decimal.TryParse(CurrentFile.ReadLine(), out mass);
+                    mass = mass / 3;
+                    mass = Math.Floor(mass);
+                    mass = mass - 2;
+
+                    Console.WriteLine(mass);
+
+                }
+            }
+        }
+    }
+}
