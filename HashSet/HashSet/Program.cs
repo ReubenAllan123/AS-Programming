e1c85100ece56bb4c8a6f6a009b1d9c8de00c433
07ReubenAllan <07ReubenAllan@IOMG21680W>
2019-12-17 13:50:14 +0000

    HashSet

---
+++ HashSet/HashSet/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashSet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           HashSet<int> numbers = new HashSet<int>() { 1, 2, 3, 4, 5, 6 };
+
+            Console.WriteLine("Adding the number 10, 8 and 2.");
+            numbers.Add(10);
+            numbers.Add(8);
+            numbers.Add(2);
+            Console.WriteLine($"Elements in HashSet: {numbers.Count}");
+
+            foreach(int num in numbers)
+            {
+                Console.WriteLine(num);
+            }
+
+            Console.ReadKey();          
+        }
+    }
+}
