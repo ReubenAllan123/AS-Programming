0e7a45fc14a26af5cb6f1163a880de532f40a319
Reuben Allan <reubenallan@online.sch.im>
2020-01-07 21:13:47 +0000

    using recursion

---
+++ Factorials/Factorials/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace factorials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 0;
+            while (number < 1)
+            {
+                Console.Write("Enter a positive integer: ");
+                int.TryParse(Console.ReadLine(), out number);
+            }
+
+            int answer = FactorialN(number);
+
+            Console.WriteLine($"Answer for {number}! is: {answer}.");
+            Console.ReadKey();
+        }
+        static int FactorialN(int number)
+        {
+            int answer;
+            if (number == 1)
+            {
+                answer = 1;
+            }
+            else
+            {
+                answer = number * FactorialN(number - 1);
+            }
+            return answer;
+        }
+    }
+}
