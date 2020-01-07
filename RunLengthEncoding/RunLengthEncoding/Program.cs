9fbb6ccd39132fcfdf28d3479ebadb17ca13058e
Reuben Allan <reubenallan@online.sch.im>
2020-01-07 21:15:24 +0000

    RLE example

---
+++ RunLengthEncoding/RunLengthEncoding/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Run_Length_Encoding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string source;
+            string compressed = "";
+            int sourceLength;
+            int timesUsed = 0;
+            char character;
+
+            Console.Write("Input source text: ");
+            source = Console.ReadLine() + " ";
+            sourceLength = source.Length;
+
+            character = source[0];
+            for (int i = 0; i < sourceLength; i++)
+            {
+                if (character == source[i])
+                {
+                    character = source[i];
+                    timesUsed++;
+                }
+                else
+                {
+                    compressed = compressed + character;
+                    compressed = compressed + timesUsed + “ “;
+
+                    character = source[i];
+                    timesUsed = 1;
+                }
+            }
+
+            Console.WriteLine(compressed);
+            Console.ReadKey();
+        }
+    }
+}
